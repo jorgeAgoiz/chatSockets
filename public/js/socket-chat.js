08c4a49c8d4d0e1dd05c10d8a2d7e12a04c11249
jorgeAP <jorgeagoiz@gmail.com>
2020-10-03 22:44:01 +0200

    JS logic updated
    03/10

---
+++ public/js/socket-chat.js
@@ -0,0 +1,52 @@
+let socket = io();
+
+let params = new URLSearchParams(window.location.search);
+
+if(!params.has('nombre') || !params.has('sala')){
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+};
+
+let usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, (resp) => {
+        console.log('Usuarios conectados', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+/* socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+}); */
+
+// Escuchar información
+socket.on('crearMensaje', (mensaje) => {
+    console.log('Servidor:', mensaje);
+});
+
+// Escuchar información
+socket.on('listaPersonas', (personas) => {
+    console.log(personas);
+});
+
+//Mensajes privados
+socket.on('mensajePrivado', (mensaje) => {
+    console.log('Mensaje privado: ', mensaje);
+});
